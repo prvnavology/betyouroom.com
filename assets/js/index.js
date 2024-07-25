dc08c5df88d09757f744cc2814939b93ba550aa2
Dev avology <devavology12@gmail.com>
2024-07-25 18:16:29 +0530

    done

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// Menu Toggle Functionality JS Start
+
+$(document).ready(function () {
+    $(".menu-icon").click(function () {
+      $("body").addClass("menuToggle");
+    });
+    $(".close-icon").click(function () {
+      $("body").removeClass("menuToggle");
+    });
+  });
+  // Menu Toggle Functionality JS End
