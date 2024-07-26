7d519d1765c1567b231bcbec4e4fde28d669abe7
Dev avology <devavology12@gmail.com>
2024-07-26 20:26:06 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,9 +23,11 @@ $(document).ready(function () {
 
 
     // Read More Functionality JS Start 
-  $('.subpage-readmore-btn').click(function(e) {
-    e.preventDefault();
-       $(this).parent().prev().slideToggle('slow');
-       $(this).text($(this).text() == 'Read Less' ? 'Read More' : 'Read Less');
-    });
+    $('.subpage-readmore-btn').click(function(e) {
+      e.preventDefault();
+      // console.log('click');
+      $(this).parent().prev().slideToggle('slow');
+      // $('.subpage-exp-content').slideToggle('slow');
+      $(this).text($(this).text() == 'Read Less' ? 'Read More' : 'Read Less');
+      });
       // Read More Functionality JS End 
