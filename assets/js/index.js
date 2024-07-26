1218a5b5121f7b734283813e5657d724ea34855d
Dev avology <devavology12@gmail.com>
2024-07-26 18:39:17 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,23 @@ $(document).ready(function () {
     });
   });
   // Menu Toggle Functionality JS End
+
+
+// Slick Slider JS Start
+  $('.home-page-cards-wrapper').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+     });
+// Slick Slider JS End
+
+
+    // Read More Functionality JS Start 
+  $('.subpage-readmore-btn').click(function(e) {
+    e.preventDefault();
+       $(this).parent().prev().slideToggle('slow');
+       $(this).text($(this).text() == 'Read Less' ? 'Read More' : 'Read Less');
+    });
+      // Read More Functionality JS End 
